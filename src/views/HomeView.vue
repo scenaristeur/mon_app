b251134b91e9bf1bc494fffa74d889634fecabe7
scenaristeur <scenaristeur@gmail.com>
2022-06-21 15:35:36 +0200

    premiere version

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Salut c'est ma super app"/>
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/mon_app/' : '/' // <-- add this line
+
+
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+	publicPath: publicPath,
   transpileDependencies: true
 })
