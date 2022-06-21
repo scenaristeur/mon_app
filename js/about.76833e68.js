ae794f20ec8a277ca0f58b20f1b486627a4915bb
scenaristeur <scenaristeur@gmail.com>
2022-06-21 15:35:36 +0200

    premiere version

---
+++ js/about.76833e68.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmon_app"]=self["webpackChunkmon_app"]||[]).push([[443],{5830:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var u=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an about page")])])}],s=t(1001),l={},r=(0,s.Z)(l,u,a,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.76833e68.js.map
