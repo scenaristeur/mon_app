4756aa9c3af93ac4729410275cbdb402405aad61
scenaristeur <scenaristeur@gmail.com>
2022-06-21 15:40:42 +0200

    Yahoo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Salut c'est ma super app"/>
+    <HelloWorld msg="Salut c'est ma super app, Yahhooo !"/>
   </div>
 </template>
 
